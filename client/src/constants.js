3901ca3909850e4080dd08330561c358f7dc2641
notchla <liso.lorenzo@gmail.com>
2021-01-26 18:27:42 +0000

    deployment changes

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:5000';
+export const API_URL = process.env.NODE_ENV === "development" ? 'http://localhost:5000' : "http://34.65.94.226";
 export const ACCOUNT_URL = `${API_URL}/account`;
 export const PROFILE_URL = `${API_URL}/profile`;
 export const CAST_URL = `${API_URL}/cast`;
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const app = require('express')();
+const express = require("express")
+const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(process.env.NODE_ENV === "development"  ? cors() : cors({origin: "http://34.65.94.226"}));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 let watchData = [];
 let playlists = [];
@@ -73,6 +76,10 @@ app.get('/watch', (req, res) => {
 app.post('/watch', (req, res) => {
   watchData.push(req.body);
   res.status(200).json(watchData);
+});
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
